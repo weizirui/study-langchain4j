3eec9f60e687f3d804f11cb3d8d57b2dbaa74e2d
weizr <544872527@qq.com>
2025-07-25 21:00:45 +0800

    基于阿里云,万象文生图功能

--- langchain4j-04low-high-api/src/main/java/com/atguigu/study/config/LLMConfig.java
+++ langchain4j-04low-high-api/src/main/java/com/atguigu/study/config/LLMConfig.java
@@ -14,40 +14,38 @@ import org.springframework.context.annotation.Configuration;
  * @Description: 知识出处 https://docs.langchain4j.dev/get-started
  */
 @Configuration
-public class LLMConfig
-{
+public class LLMConfig {
     @Bean(name = "qwen")
-    public ChatModel chatModelQwen()
-    {
+    public ChatModel chatModelQwen() {
         return OpenAiChatModel.builder()
-                    .apiKey(System.getenv("DASH_SCOPE_API_KEY"))
-                    .modelName("qwen-plus")
-                    .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
+                .apiKey(System.getenv("DASH_SCOPE_API_KEY"))
+                .modelName("qwen-plus")
+                .logRequests(true)
+                .logResponses(true)
+                .maxRetries(3)
+                .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
                 .build();
     }
 
     /**
-    * @Description: 知识出处，https://api-docs.deepseek.com/zh-cn/
-    * @Auther: zzyybs@126.com
-    */
+     * @Description: 知识出处，https://api-docs.deepseek.com/zh-cn/
+     * @Auther: zzyybs@126.com
+     */
     @Bean(name = "deepseek")
-    public ChatModel chatModelDeepSeek()
-    {
+    public ChatModel chatModelDeepSeek() {
         return
                 OpenAiChatModel.builder()
                         .apiKey(System.getenv("DEEP_SEEK_API_KEY"))
                         .modelName("deepseek-chat")
                         //.modelName("deepseek-reasoner")
                         .baseUrl("https://api.deepseek.com/v1")
-                .build();
+                        .build();
     }
-
 
 
     // High-Api https://docs.langchain4j.dev/tutorials/ai-services#simplest-ai-service
     @Bean
-    public ChatAssistant chatAssistant(@Qualifier("qwen") ChatModel chatModelQwen)
-    {
+    public ChatAssistant chatAssistant(@Qualifier("qwen") ChatModel chatModelQwen) {
         return AiServices.create(ChatAssistant.class, chatModelQwen);
     }
 }
